bd85adfd3e451211edf3310c866953573973b75e
wagner <wagnersr92@gmail.com>
2020-12-25 17:02:59 -0300

    Ajuste da API_KEY que estava errada

--- app/src/main/java/wagnerrodrigues/io/newsappstarter/util/Constantes.kt
+++ app/src/main/java/wagnerrodrigues/io/newsappstarter/util/Constantes.kt
@@ -2,7 +2,7 @@ package wagnerrodrigues.io.newsappstarter.util
 
 class Constantes {
     companion object{
-        const val API_KEY = "d45cc94d24d4246a0a9ddf6120096b8"
+        const val API_KEY = "4d45cc94d24d4246a0a9ddf6120096b8"
         const val BASE_URL = "https://newsapi.org"
     }
 }
